481723db447194434239eb171e8deac6984aaff2
Darllancabral <cabraldarllan@gmail.com>
2025-05-15 18:57:22 -0300

    correção3-7

--- LISTAR_POR_NOME.java
+++ LISTAR_POR_NOME.java
@@ -1,17 +1,18 @@
-
 import javax.swing.*;
 import java.util.*;
 
 public class LISTAR_POR_NOME {
-    public static void listarPorNome(
-            ArrayList<String> titulos,
-            ArrayList<String> autores,
-            ArrayList<Integer> anoPublicacao,
-            ArrayList<String> status,
-            ArrayList<String> leitores) {
+    public static void listarPorNome(ArrayList<String> titulos, ArrayList<String> autores,
+                                     ArrayList<Integer> anoPublicacao, ArrayList<String> status,
+                                     ArrayList<String> livrosEmprestados, ArrayList<String> livrosDevolvidos) {
 
         // Entrada do usuário
         String chamada = JOptionPane.showInputDialog("Digite o título do livro para buscar:");
+
+        if (chamada == null || chamada.trim().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Você deve digitar um título para buscar.");
+            return;
+        }
 
         if (titulos.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Não há livros cadastrados.");
@@ -23,16 +24,17 @@ public class LISTAR_POR_NOME {
 
         for (int item = 0; item < titulos.size(); item++) {
             if (titulos.get(item).toLowerCase().contains(chamada.toLowerCase())) {
+                resultado += "Código: " + (item + 1) + "\n";
                 resultado += "Título: " + titulos.get(item) + "\n";
                 resultado += "Autor: " + autores.get(item) + "\n";
                 resultado += "Ano: " + anoPublicacao.get(item) + "\n";
                 resultado += "Status: " + status.get(item) + "\n";
 
                 if (status.get(item).equals("Emprestado")) {
-                    resultado += "Leitor: " + leitores.get(item) + "\n";
+                    resultado += "Leitor: " + livrosEmprestados.get(item) + "\n";
                 }
 
-                resultado += "--------------------------\n";
+                resultado += "________________________\n";
                 encontrou = true;
             }
         }
@@ -42,6 +44,5 @@ public class LISTAR_POR_NOME {
         } else {
             JOptionPane.showMessageDialog(null, "Nenhum livro encontrado com esse título.");
         }
-
     }
 }
